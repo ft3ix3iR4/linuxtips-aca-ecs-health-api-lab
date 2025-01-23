19443306391cdfd9a0f32bbf8b4d126265e20f68
ft3ix3iR4 <fellipewolly2@gmail.com>
2025-01-23 07:24:40 -0300

    feat: primeiro commit

---
+++ variable.tf
@@ -0,0 +1,51 @@
+variable "cluster_name" {
+  default = "linuxtips-ecs-cluster"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ssm_alb" {
+  default = "/linuxtips/ecs/lb/id"
+}
+
+variable "ssm_listener" {
+  default = "/linuxtips/ecs/lb/listener"
+}
+
+variable "ssm_alb_internal" {
+  default = "/linuxtips/ecs/lb/internal/id"
+}
+
+variable "ssm_listener_internal" {
+  default = "/linuxtips/ecs/lb/internal/listener"
+}
+
+variable "ssm_vpc_id" {
+  default = "/linuxtips-vpc/vpc/vpc_id"
+}
+
+variable "ssm_private_subnet_1" {
+  default = "/linuxtips-vpc/vpc/private_subnet_1a"
+}
+
+variable "ssm_private_subnet_2" {
+  default = "/linuxtips-vpc/vpc/private_subnet_1b"
+}
+
+variable "ssm_private_subnet_3" {
+  default = "/linuxtips-vpc/vpc/private_subnet_1c"
+}
+
+variable "ssm_service_discovery_namespace" {
+  default = "/linuxtips/ecs/cloudmap/namespace"
+}
+
+variable "ssm_service_connect_arn" {
+  default = "/linuxtips/ecs/service-connect/namespace"
+}
+
+variable "ssm_service_connect_name" {
+  default = "/linuxtips/ecs/service-connect/name"
+}
